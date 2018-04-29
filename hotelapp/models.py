e6b3c9f274f3fd7ae7ade71c3519b4fad21c5669
Scott Hill <scott289@gmail.com>
2018-04-28 17:41:15 -0700

    makes a res

--- hotelapp/models.py
+++ hotelapp/models.py
@@ -9,14 +9,22 @@ class Hotel(models.Model):
     def __str__(self):
         return self.name
 
+class ReservationManager(models.Manager):
+    def create_res(self, hotel, customer, date_res):
+        res = self.create(hotel=hotel, customer=customer, date_res=date_res)
+        return res
+        
 class Reservation(models.Model):
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
     customer = models.CharField(max_length=100)    
     date_res = models.DateField('Date of the stay.')
     date_updated = models.DateTimeField(auto_now=True)
+    objects = ReservationManager()
 
     def is_current(self):
         return self.date_res >= timezone.now()
 
     def __str__(self):
         return self.date_res.strftime("%Y-%m-%d") + ' at ' + self.hotel.name + ' for ' + self.customer
+
+
--- hotelapp/views.py
+++ hotelapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Hotel
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Hotel, Reservation
 
 def index(request):
     hotel_list = Hotel.objects.all()
@@ -16,5 +17,9 @@ def hotel_reservations(request, hotel_id):
     return HttpResponse(response % hotel_id)
 
 def reserve(request, hotel_id):
-    return HttpResponse("You're reserving for hotel %s." % hotel_id)
+    hotel = get_object_or_404(Hotel, pk=hotel_id)
+    res_date = request.POST['res_date']
+    customer = request.POST['customer']
+    reservation = Reservation.objects.create_res(hotel, customer, res_date)
+    return HttpResponseRedirect(reverse('hotelapp:hotel_detail', args=(hotel.id,)))
 
