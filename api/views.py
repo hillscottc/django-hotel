410107daa7c83129cb281e025699f8af9f3ee18e
Scott Hill <scott289@gmail.com>
2018-05-04 15:49:36 -0700

    Using generic class-based views

--- api/views.py
+++ api/views.py
@@ -1,101 +1,35 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework import status
-from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import HotelSerializer, ReservationSerializer
 from .models import Hotel, Reservation
 
-# I can make these much smaller with <http://www.django-rest-framework.org/tutorial/3-class-based-views/#using-generic-class-based-views>,
-# if I can make sure the validation is working at the model level. Otherwise, I have to write it here.
 
-
-class HotelList(APIView):
+class HotelList(generics.ListCreateAPIView):
     """
     List all hotels, or create a new hotel.
     """
-    def get(self, request, format=None):
-        hotels = Hotel.objects.all()
-        serializer = HotelSerializer(hotels, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = HotelSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
 
 
-class HotelDetail(APIView):
+class HotelDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a hotel instance.
+    Retrieve, update, or delete a hotel instance.
     """
-    def get_object(self, pk):
-        try:
-            return Hotel.objects.get(pk=pk)
-        except Hotel.DoesNotExist:
-            raise Http404
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
 
-    def get(self, request, pk, format=None):
-        hotel = self.get_object(pk)
-        serializer = HotelSerializer(hotel)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        hotel = self.get_object(pk)
-        serializer = HotelSerializer(hotel, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        hotel = self.get_object(pk)
-        hotel.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
     
-    
-class ReservationList(APIView):
+class ReservationList(generics.ListCreateAPIView):
     """
     List all reservations, or create a new reservation.
     """
-    def get(self, request, format=None):
-        reservations = Reservation.objects.all()
-        serializer = ReservationSerializer(reservations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ReservationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
 
     
-class ReservationDetail(APIView):
+class ReservationDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a reservation instance.
     """
-    def get_object(self, pk):
-        try:
-            return Reservation.objects.get(pk=pk)
-        except Reservation.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        reservation = self.get_object(pk)
-        serializer = ReservationSerializer(reservation)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        reservation = self.get_object(pk)
-        serializer = ReservationSerializer(reservation, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        reservation = self.get_object(pk)
-        reservation.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
